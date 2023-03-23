76d2accde15ed6e63fe28552ebb2ddcf80c89fa3
Julien Cheritat <julien.cheritat@neos-sdi.com>
2023-03-23 22:10:42 +0100

    Add a middleware Demonstrate middleware an its relation to routing

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,11 +11,14 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Primitives;
+using System.Buffers;
 using System.Diagnostics;
+using System.Text;
 using System.Text.Json;
+using System.Text.Unicode;
 
-var builder = WebHost.CreateDefaultBuilder<MyStartup>(args);
-// var builder = WebApplication.CreateBuilder(args);
+// var builder = WebHost.CreateDefaultBuilder<MyStartup>(args);
+var builder = WebApplication.CreateBuilder(args);
 // var builder = new WebHost();
 // builder
 //     .UseContentRoot(Directory.GetCurrentDirectory())
@@ -34,6 +38,30 @@ var builder = WebHost.CreateDefaultBuilder<MyStartup>(args);
 
 var app = builder.Build();
 
+app.UseRouting();
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Method != "GET")
+    {
+        context.Response.StatusCode = 400;
+        context.Response.BodyWriter.Write(Encoding.UTF8.GetBytes("Invalid Request"));
+        return;
+    }
+
+    if (context.GetEndpoint()?.Metadata.GetMetadata<RequiresAuditAttribute>() is not null)
+    {
+        Console.WriteLine($"ACCESS TO SENSITIVE DATA AT: {DateTime.UtcNow}");
+    }
+
+    await next(context);
+});
+
+app.MapGet("/", () => "Audit isn't required.");
+app.MapGet("/sensitive", () => "Audit required for sensitive data.")
+    .WithMetadata(new RequiresAuditAttribute());
+
+
 await app.StartAsync();
 await app.WaitForShutdownAsync();
 internal class MyStartup : IStartup
@@ -46,4 +74,11 @@ internal class MyStartup : IStartup
 
     IServiceProvider IStartup.ConfigureServices(IServiceCollection services) =>
             services.BuildServiceProvider();
+}
+
+internal class RequiresAuditAttribute : Attribute
+{
+    public RequiresAuditAttribute()
+    {
+    }
 }
